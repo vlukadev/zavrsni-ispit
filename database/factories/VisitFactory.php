3646fc183c83cb974a1cfcfd4e44f3dab20ddc9e
Luka Vasiljevic <lukaxman2@gmail.com>
2023-11-27 10:19:38 +0100

    deployUpdate

---
+++ database/factories/VisitFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Visit>
+ */
+class VisitFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+            'product_id' => $this->faker->numberBetween(1,150),
+        ];
+    }
+}
